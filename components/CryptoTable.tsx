04bf4517d4efbe02a8b931143b4b03b58b96fd5e
hasnain <hasnain@kali.hasnain>
2025-04-28 03:14:52 +0500

    u

--- components/CryptoTable.tsx
+++ components/CryptoTable.tsx
@@ -57,9 +57,9 @@ export default function CryptoTable() {
   );
 
   return (
-    <div className=" p-4 rounded-lg w-full max-w-sm text-white">
+    <div className="rounded-lg w-full max-w-sm text-white">
       {/* Search */}
-      <div className="flex items-center bg-transparent px-3 py-2 rounded-2xl mb-4 border-2 border-gray-800">
+      <div className="flex items-center bg-transparent px-3 py-2 rounded-lg mb-4 border-2 border-gray-800">
         <span className="text-[#f0b90b] mr-2 text-lg">
           <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M16.6355 15.3927L12.7529 11.2775C13.7512 10.0681 14.2982 8.54645 14.2982 6.96234C14.2982 3.26121 11.3434 0.25 7.71158 0.25C4.0798 0.25 1.125 3.26121 1.125 6.96234C1.125 10.6635 4.0798 13.6747 7.71158 13.6747C9.075 13.6747 10.3743 13.2556 11.4851 12.46L15.3973 16.6065C15.5608 16.7796 15.7807 16.875 16.0164 16.875C16.2395 16.875 16.4511 16.7883 16.6118 16.6307C16.9531 16.296 16.964 15.7409 16.6355 15.3927ZM7.71158 2.00104C10.396 2.00104 12.5799 4.22662 12.5799 6.96234C12.5799 9.69805 10.396 11.9236 7.71158 11.9236C5.02712 11.9236 2.84324 9.69805 2.84324 6.96234C2.84324 4.22662 5.02712 2.00104 7.71158 2.00104Z" fill="#EDB546" />
@@ -71,7 +71,7 @@ export default function CryptoTable() {
           placeholder="Search"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          className="bg-transparent outline-none text-sm placeholder-white flex-1"
+          className="bg-transparent outline-none text-sm placeholder-white placeholder:text-[12px] flex-1"
         />
       </div>
 
